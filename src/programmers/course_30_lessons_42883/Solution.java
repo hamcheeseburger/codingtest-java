b462ced46784319f9b74a07462e65dd34fd00b38
corinne <yoo77hyeon@gmail.com>
2025-10-20 10:02:35 +0900

    feat : course 30 lesson 42883 문제 풀이

---
+++ src/programmers/course_30_lessons_42883/Solution.java
@@ -0,0 +1,54 @@
+package programmers.course_30_lessons_42883;
+
+import java.util.Arrays;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42883
+public class Solution {
+    public String solution(String number, int k) {
+        // 이건 greedy로 푼게 아니다. 시간효율성 실패
+        StringBuilder answer = new StringBuilder();
+
+        int[] numbers = Arrays.stream(number.split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] newNumbers = new int[numbers.length];
+
+        Arrays.fill(newNumbers, -1);
+
+        for (int count = 0; count < numbers.length - k; count++) {
+            long max = 0;
+            int maxIndex = -1;
+            for (int i = 0; i < numbers.length; i++) {
+                if (newNumbers[i] != -1) {
+                    continue;
+                }
+                newNumbers[i] = numbers[i];
+                StringBuilder s = new StringBuilder();
+                for (int n : newNumbers) {
+                    if (n != -1) {
+                        s.append(n);
+                    }
+                }
+
+                long mediumValue = Long.parseLong(s.toString());
+                if (max <= mediumValue) {
+                    max = mediumValue;
+                    maxIndex = i;
+                }
+                newNumbers[i] = -1;
+            }
+
+            if (maxIndex != -1) {
+                newNumbers[maxIndex] = numbers[maxIndex];
+            }
+        }
+
+        for (int newNumber : newNumbers) {
+            if (newNumber != -1) {
+                answer.append(newNumber);
+            }
+        }
+
+        return answer.toString();
+    }
+}
---
+++ src/programmers/course_30_lessons_42883/Solution_Corrected_2.java
@@ -0,0 +1,41 @@
+package programmers.course_30_lessons_42883;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Solution_Corrected_2 {
+
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+
+        int[] numbers = Arrays.stream(number.split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(numbers[0]);
+        int removed = 0;
+
+        for (int i = 1; i < numbers.length; i++) {
+            while (removed < k && !stack.isEmpty() && stack.peek() < numbers[i]) {
+                stack.pop();
+                removed++;
+            }
+            stack.push(numbers[i]);
+        }
+
+        if (numbers.length == stack.size()) {
+            removed = 0;
+            while (removed < k) {
+                stack.pop();
+                removed++;
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            answer.insert(0, stack.pop());
+        }
+
+        return answer.toString();
+    }
+}
